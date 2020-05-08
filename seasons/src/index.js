037638d1cb8d4af93886440ec2960454ed645337
Pete Russell <pinkbatz@gmail.com>
2020-05-08 14:09:10 +1200

    Modified: move location code to componentDidMount() (best practice).

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = { lat: null, errorMessage: '' };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errorMessage: err.message })
