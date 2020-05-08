8c6670233ad63bb577703b4a8a7603a36a9cda87
Pete Russell <pinkbatz@gmail.com>
2020-05-08 17:45:44 +1200

    Added: HTTP request with axios and handle response.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit (value) {
-        console.log(`app: onSubmit ${value}`);
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 1ZoMUz9uJMD9V_7n3_6miaDyEu7IWAWtCPQpGyPX91k'
+            }
+        });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.length} images.
             </div>
         );
     }
