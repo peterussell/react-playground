4d4c7013f9edfff9eb954114ee0941dc3d99c293
Pete Russell <pinkbatz@gmail.com>
2020-05-10 18:13:20 +1200

    Added: example of connecting and using action creators.

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
     renderList() {
@@ -7,7 +8,10 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
                             Select
                         </button>
                     </div>
@@ -32,12 +36,22 @@ class SongList extends Component {
 // called mapStateToProps by convention
 // state contains all state in the entire store
 const mapStateToProps = (state) => {
+    console.log(state);
     // the object we return is what shows up as props on the component
     // ie. (inside SongList): this.props === { songs: state.songs }
     return { songs: state.songs };
 };
 
+// first arg: which pieces of state we want
+// second arg: an object, containing the actions we want access to, and the
+// props values to map them to (NB below uses shorthand for { selectSong: selectSong })
+//   NB. calling these action creators will create the action object,
+//   BUT ALSO dispatch the action automatically (ie. call dispatch on redux)
+
 // NB. this syntax is:
 // - connect returns a function
 // - then invoke that function once returned
-export default connect(mapStateToProps)(SongList);
+export default connect(
+    mapStateToProps,
+    { selectSong }
+)(SongList);
