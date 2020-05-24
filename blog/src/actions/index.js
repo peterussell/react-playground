204a2d59fd148ec71b07b6a685ef2afa6dbcfb2f
Pete Russell <pinkbatz@gmail.com>
2020-05-24 13:35:37 +1200

    Added: fix overfetching issue using lodash memoization.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (userId) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${userId}`);
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
