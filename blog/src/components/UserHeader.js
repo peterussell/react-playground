50305c67ea1a8a094207182ac066b186e3e94e68
Pete Russell <pinkbatz@gmail.com>
2020-05-24 14:40:40 +1200

    Modified: fetch users without overfetching using more generic approach (no memoization).

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
     if (!user) {
@@ -26,7 +21,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user };
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps, null)(UserHeader);
